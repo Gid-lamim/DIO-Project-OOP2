b73dfc2977346965018f951641531877640d68fa
EuMesmo <gid.lamim@gmail.com>
2022-03-22 13:46:49 -0300

    second

--- src/App.java
+++ src/App.java
@@ -4,6 +4,10 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("as");
+		
+		Endereco endereco = new Endereco("Ordalia Albino", 585, "", "Santa Claudia");
+		
+		System.out.println(endereco);
 	}
 
 }
